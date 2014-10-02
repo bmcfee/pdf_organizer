ecb58190421c21b07634d34ce9baa808f8fe6f71
Brian McFee <brian.mcfee@nyu.edu>
2014-10-02 16:29:14 -0400

    Initial checkin

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='pdf_organizer',
+    version='0.1',
+    description='A simple PDF sorting tool',
+    author='Brian McFee',
+    author_email='brian.mcfee@nyu.edu',
+    url='http://github.com/bmcfee/pdf_organizer',
+    download_url='http://github.com/bmcfee/pdf_organizer/releases',
+    long_description="""\
+        A simple PDF sorting tool
+    """,
+    scripts=['pdf_organizer'],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+    ],
+    keywords='pdf',
+    license='GPL',
+    install_requires=[
+        'PyPDF2',
+    ],
+)
